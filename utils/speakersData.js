9423175b853e91947bcee2eb7c93572e8b38b238
Vinayak Das <vinayakd909@gmail.com>
2022-01-24 21:22:04 +0530

    Add Speaker

--- utils/speakersData.js
+++ utils/speakersData.js
@@ -1,9 +1,16 @@
 import Img1 from "../public/speakers/1.jpg"
 import Img2 from "../public/speakers/2.jpg"
+import Img3 from "../public/speakers/3.jpg"
 
 export const SpeakersData =[
     
-    
+    {
+        name:"Nikita Gandhi ",
+        img:Img2,
+        title:"Community Manager at Google Developers",
+        linkedIn: "https://www.linkedin.com/in/nikita-gandhi01/",
+        twitter: "https//twitter.com/",
+    },
    
     {
         name:"Usha Rengaraju ",
@@ -12,15 +19,14 @@ export const SpeakersData =[
         linkedIn: "https://www.linkedin.com/in/usha-rengaraju-b570b7a2/",
         twitter: "https//twitter.com/",
     },
-   
-   
+
     {
-        name:"Nikita Gandhi ",
-        img:Img2,
-        title:"Community Manager at Google Developers",
-        linkedIn: "https://www.linkedin.com/in/nikita-gandhi01/",
-        twitter: "https//twitter.com/",
-    },
+        name:"Shreyas Patil",
+        img:Img3,
+        title:"Google Developer Expert",
+        linkedIn:"https://www.linkedin.com/in/patil-shreyas/"
+    }
+   
     // {
     //     name:"Nikita Gandhi 1",
     //     title:"Community Manager at Google Developers",
