4a5aa204394ef010094e5ea8a29742b0082b0fab
Vinayak Das <vinayakd909@gmail.com>
2022-01-25 20:46:40 +0530

    Add Speakers

--- utils/speakersData.js
+++ utils/speakersData.js
@@ -1,12 +1,16 @@
 import Img1 from "../public/speakers/1.jpg"
 import Img2 from "../public/speakers/2.jpg"
 import Img3 from "../public/speakers/3.jpg"
+import Img4 from "../public/speakers/4.jpg"
+import Img5 from "../public/speakers/5.jpg"
+import Img6 from "../public/speakers/6.jpg"
+import Img7 from "../public/speakers/7.jpg"
 
 export const SpeakersData =[
     
     {
         name:"Nikita Gandhi ",
-        img:Img2,
+        img:Img1,
         title:"Community Manager at Google Developers",
         linkedIn: "https://www.linkedin.com/in/nikita-gandhi01/",
         twitter: "https//twitter.com/",
@@ -14,7 +18,7 @@ export const SpeakersData =[
    
     {
         name:"Usha Rengaraju ",
-        img:Img1,
+        img:Img2,
         title:"Data Scientist",
         linkedIn: "https://www.linkedin.com/in/usha-rengaraju-b570b7a2/",
         twitter: "https//twitter.com/",
@@ -25,7 +29,35 @@ export const SpeakersData =[
         img:Img3,
         title:"Google Developer Expert",
         linkedIn:"https://www.linkedin.com/in/patil-shreyas/"
-    }
+    },
+
+    {
+        name:"Harsh B. Mishra",
+        img:Img4,
+        title:"Software Engineer Quansight",
+        linkedIn:"https://www.linkedin.com/in/harshcasper/"
+    },
+
+    {
+        name:"Kamal S.",
+        img:Img5,
+        title:"Google Dev Expert - Flutter",
+        linkedIn:"https://www.linkedin.com/in/kamalshree/"
+    },
+
+    {
+        name:"Arsh Goyal",
+        img:Img6,
+        title:"Senior Software Engineer Samsung",
+        linkedIn:"https://www.linkedin.com/in/arshgoyal/"
+    },
+
+    {
+        name:"Sishir Kalita",
+        img:Img7,
+        title:"Data Scientist Armsoftech.air",
+        linkedIn:"https://www.linkedin.com/in/sishir-kalita-3a006120/"
+    },
    
     // {
     //     name:"Nikita Gandhi 1",
