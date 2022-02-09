2ca8e0f79bf0c8213215a9a908d5f1cfb7db7e1f
Vinayak Das <vinayakd909@gmail.com>
2022-02-09 21:30:24 +0530

    Thanks Structure

--- pages/[speakerName].js
+++ pages/[speakerName].js
@@ -1,12 +1,24 @@
+import Navbar from "../components/Navbar";
+import ThanksUpper from "../components/ThanksUpper";
+import { useState } from 'react'
+
 export default function ThankYou(){
+    const [toggle, setToggle] = useState(false);
+  const toggleSidebar = () => {
+    setToggle(!toggle);
+  }
     return(
         <>
+            <Navbar toggleSidebar={toggleSidebar} />
+
+            <main className={toggle ? 'h-[80vh] overflow-hidden':""}>
             <div className="thanks-top">
-                Content here
+               <ThanksUpper />
             </div>
             <div className="thanks-bottom">
                 Content here
             </div>
+            </main>
         </>
     )
 }
