053a83fc447638f6965831547d3341d570a91ae5
Vinayak Das <vinayakd909@gmail.com>
2022-02-09 22:24:30 +0530

    Add Thanks Top

--- components/ThanksUpper.js
+++ components/ThanksUpper.js
@@ -1,7 +1,22 @@
+import Image from "next/image"
+import thanksHeading from "../public/thanksHeading.svg"
+import techStackLogos from "../public/techStackLogos.svg"
+import topBlue from "../public/topBlue.svg"
+
 export default function ThanksUpper(){
     return(
         <>
-            
+            <div className="flex flex-col md:flex-row md:flex mt-[3rem]">
+                <div className="img1">
+                <Image src={thanksHeading} height={300} />
+                </div>
+                <div className="img2">
+                <Image src={techStackLogos} height={400} />
+                </div>
+            </div>
+            <div className="bottomImg">
+                    <Image src={topBlue} />
+                </div>
         </>
     )
 }
