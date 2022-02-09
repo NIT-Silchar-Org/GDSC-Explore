0b6176d4b08e9c85e69bce653544e6342feedbfc
Vinayak Das <vinayakd909@gmail.com>
2022-02-09 12:59:45 +0530

    Add Thank You Page Structure

---
+++ pages/[speakerName].js
@@ -0,0 +1,12 @@
+export default function ThankYou(){
+    return(
+        <>
+            <div className="thanks-top">
+                Content here
+            </div>
+            <div className="thanks-bottom">
+                Content here
+            </div>
+        </>
+    )
+}
